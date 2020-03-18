29146ce9eb791f1911add20f7f0234bed6b44062
Nico Mingkun Xia <49419222+nicoxmk@users.noreply.github.com>
2020-03-18 15:11:17 -0400

    Add files via upload
    header file

---
+++ Tic-Tac-Toe/functions.h
@@ -0,0 +1,12 @@
+//Nico Mingkun Xia 
+#ifndef _FUNCTIONS_
+#define _FUNCTIONS_
+
+void greetAndInstruct();
+void displayBoard(char board[]);
+void displayLayout(int board[]);
+bool checkIfLegal(int cellNbre, char board[]);
+bool checkWinner(char board[]);
+void computerMove(char board[]);
+
+#endif
